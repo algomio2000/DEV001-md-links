8cdf2fb78e97bbe8f77b04aed434c2e9645a0310
Silvana Gonzalez <algomio2000@gmail.com>
2023-01-22 21:29:08 -0500

    Preparando la funcion para extaer los links

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const fs = require ('fs');
-const path = require ('path'); 
+const path = require ('path');
+const marked = require('marked'); 
 // comprobando si la ruta es valida
 const isValidRoute = (route) => {
   if (fs.existsSync(route)) {
@@ -20,13 +21,41 @@ const getAbsoluteRoute = (route) => {
   }
 };
 
-//averiguar si es archivo
+
+//averiguar si es archivo o directorio
 const pathIsDirectory = (ruta) => {
   return fs.lstatSync(ruta).isDirectory()
-}
+}// payhIsDirectorio es true de volver un cacth, por ahora no estamos leyendo directorios
 //averiguar si es archivo
 const pathIsFile = function (ruta){
   return fs.statSync(ruta).isFile()
-}
+} 
 
-console.log (pathIsFile);
+// si pathIsFile es true averiguo si es un archivo md, si es false debo crear un catch
+const isMdFile = (route) => (path.extname(route) === '.md');
+
+const getMdFiles = (routeFile) => {
+  let arrayMdFile = [];
+  // Utilizo la función getAbsoluteRoute() para obtener la ruta absoluta de la ruta dada.
+  const route = getAbsoluteRoute(routeFile);
+  // Luego usa la función pathIsFile() para verificar si la ruta es un archivo. Si es un archivo, 
+  //se verifica si es un archivo Markdown usando la función isMdFile
+  if (pathIsFile(route)) {
+    if (isMdFile(route)) {
+      //Si es un archivo Markdown, se agrega al array de archivos Markdown
+      arrayMdFile.push(route);
+    }
+  } else {
+    console.error('Error: no es un archivo');
+    
+    };
+    return arrayMdFile;
+  }
+  //leer archivos y extraer los links
+  const getLinksMd = (routeFile)=>{
+    let arrayResults = [];
+    const arrayMdFile;
+  }
+  /*debo tener un array vacio para llenar con los links, debotener el array de archivos md, la funcion
+  o metodo parfa extarer los links*/
+
