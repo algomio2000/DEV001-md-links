11b2cf7f4c86eda47ea2d9dbd1965d5ecd5b8952
silvana Gonzalez <algomio2000@gmail.com>
2023-02-06 11:57:50 -0500

    Tratande de corregir errores

--- main.js
+++ main.js
@@ -29,10 +29,10 @@ const getAbsoluteRoute = (inputpath) => {
         return false;
     }
   };
-
-  // FUNCION PARA SABER SI ES UN ARCHIVO .MD
-const  isMdFile  =  ( inputpath )  =>  ( inputpath . extname ( inputpath)  ===  '.md' ) ;
-   
+console.log(pathIsFile('DEV001-md-links\test\exampleFiles\ejemplo2.md'));
+  
+// FUNCION PARA SABER SI ES UN ARCHIVO .MD
+  const isMdFile = (route) => (path.extname(route) === '.md');   
 //
   const getMdFiles = (routeFile) => {
     let arrayMdFile = [];
@@ -41,19 +41,11 @@ const  isMdFile  =  ( inputpath )  =>  ( inputpath . extname ( inputpath)  ===  
       if (isMdFile(route)) {
         arrayMdFile.push(route);
       }
-      else {
-        const arrayOfFiles = fs.readdirSync(route);
-        arrayOfFiles.forEach((file) => {
-          const arrayMd = getMdFiles(path.join(route, file));
-          arrayMdFile = arrayMdFile.concat(arrayMd);
-        });
-      }
-    } 
-    
-  };
-  console.log(getMdFiles('C:\Users\TecnoBot\Desktop\md links\DEV001-md-links\test\exampleFiles\example.md')); 
+       };
+      };
+  //console.log(getMdFiles('C:\Users\TecnoBot\Desktop\md links\DEV001-md-links\test\exampleFiles\ejemplo2.md')); 
   // FUNCION PARA RUTAS ABSOLUTAS DE LOS ARCHIVOS ENCONTRADOS
-/*const getLinksMd = (inputpath )=> {
+const getLinksMd = (inputpath )=> {
   const arrayMdFiles = getMdFiles(inputpath);
   //console.log(arrayMdFiles);
   const renderer = new marked.Renderer();
@@ -74,9 +66,9 @@ const  isMdFile  =  ( inputpath )  =>  ( inputpath . extname ( inputpath)  ===  
   });
   return arrayofLinks;
 };
-console.log(getLinksMd('./exampleFiles/exampleFile.md'));
+//console.log(getLinksMd('C:\Users\TecnoBot\Desktop\md links\DEV001-md-links\test\exampleFiles\ejemplo2.md'));
 
-  //console.log(getLinksMd('\DEV001-md-links\\exampleFiles\\exampleFile.md'));*/
+  //console.log(getLinksMd('\DEV001-md-links\\exampleFiles\\exampleFile.md'));
 
   module.exports = {
     getAbsoluteRoute,
