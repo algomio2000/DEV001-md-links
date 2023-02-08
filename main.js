57e4158897cc437fed50ab45aa40d496d43a0f5c
silvana Gonzalez <algomio2000@gmail.com>
2023-02-08 13:16:42 -0500

    Funcion getLinksmd resuelta

--- main.js
+++ main.js
@@ -12,10 +12,6 @@ const getAbsoluteRoute = (inputpath) => {
     return path.resolve(inputpath);
   }
 };
-//console.log(getAbsoluteRoute('C:Users\\TecnoBot\\Desktop\\md links\\DEV001-md-links\\test\\exampleFiles\\ejemplo2.md'));
-
-
-//averiguar si es archivo
 
 //averiguar si es un archivo
 const pathIsFile = (inputpath) => fs.statSync(inputpath).isFile();
@@ -25,7 +21,7 @@ const pathIsFile = (inputpath) => fs.statSync(inputpath).isFile();
 const isMdFile = (route) => (path.extname(route) === '.md');
 //console.log(isMdFile('test\exampleFiles\example.md'));
 
-//
+//los archivos Md ponerlos en un array
 const getMdFiles = (routeFile) => {
   let arrayMdFile = [];
   const route = getAbsoluteRoute(routeFile);
@@ -43,7 +39,7 @@ const getMdFiles = (routeFile) => {
 //console.log(getMdFiles('test\\exampleFiles\\ejemplo2.md')); 
 
 // FUNCION PARA RUTAS ABSOLUTAS DE LOS ARCHIVOS ENCONTRADOS
-/*const getLinksMd = (route) => {
+const getLinksMd = (route) => {
   const arrayMdFiles = getMdFiles(route);
   const renderer = new marked.Renderer();
   const arrayofLinks = [];
@@ -57,26 +53,13 @@ const getMdFiles = (routeFile) => {
         path: filePath,
       });
     };
-    marked(file, { renderer });
+    marked.marked(file, { renderer });
   });
   return arrayofLinks;
-};*/const getLinksMd= (inputhpath)=>{
-const renderer = new marked.Renderer();
-const arrayofLinks = [];
-
-renderer.link = (urlFile, _, urlText) => {
-  arrayofLinks.push({
-    href: urlFile,
-    text: urlText,
-  });
 };
-};
-marked(file, { renderer });
 
-return arrayofLinks;
+
 console.log(getLinksMd('test\\exampleFiles\\ejemplo2.md'));
-
-//console.log(getLinksMd('\DEV001-md-links\\exampleFiles\\exampleFile.md'));
 
 module.exports = {
   getAbsoluteRoute,
